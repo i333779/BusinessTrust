49820ddaa5478a973958d05ed7c321ba097dbf05
Kumari <kritika.kumari@sap.com>
2020-06-01 02:11:31 +0530

    axios

--- controllers/EmployeeFormController.js
+++ controllers/EmployeeFormController.js
@@ -1,8 +1,10 @@
 var https = require('follow-redirects').https;
 var fs = require('fs');
+const axios = require('axios');
 var express = require('express');
 var router = express.Router();
 var funcPostData = {};
+
 
 
 class EmployeeFormController {
@@ -36,40 +38,24 @@ class EmployeeFormController {
         console.log(body.toString());
         console.log(JSON.parse(body).data[0].inContainmentZone);
         funcPostData["Location"] = JSON.parse(body).data[0].inContainmentZone;
-
-        var options = {
-          'method': 'POST',
-          'hostname': 'businesstrustfunctionapp.azurewebsites.net',
-          'path': '/api/BusinessTrustFuncApp',
-          'headers': {
-            'Content-Type': 'text/plain'
-          },
-          'maxRedirects': 20
-        };
-
-        var req = https.request(options, function (res) {
-          var chunks = [];
-
-          res.on("data", function (chunk) {
-            chunks.push(chunk);
+       axios.post('https://businesstrustfunctionapp.azurewebsites.net/api/BusinessTrustFuncApp', {
+        EmpId : funcPostData["EmpId"],
+        SafetyGear : funcPostData["SafetyGear"] ,
+        SafeDistance : funcPostData["SafeDistance"] ,
+        Temperature : funcPostData["Temperature"],
+        Location : funcPostData["Location"] 
+        })
+          .then((res) => {
+            console.log(`statusCode: ${res.statusCode}`)
+            console.log(res)
+          })
+          .catch((error) => {
+            console.error(error)
           });
-
-          res.on("end", function (chunk) {
-            var body = Buffer.concat(chunks);
-            console.log(body.toString());
-          });
-
-          res.on("error", function (error) {
-            console.error(error);
-          });
-        });
-
-        funcPostData = JSON.stringify(funcPostData);
-        console.log(funcPostData);
-        req.write(funcPostData);
-        req.end();
 
       });
+
+
 
       res.on("error", function (error) {
         console.error(error);
